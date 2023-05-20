e28f61db92d6893d6d720edd73614629cd652f14
骚皮耗子 <118991670+SPHZ-LH@users.noreply.github.com>
2023-05-21 01:44:17 +0800

    Delete CollecTionDemo9.java

--- CollecTionDemo9.java
+++
@@ -1,9 +0,0 @@
-public class CollecTionDemo9 {
-    public static void main(String[] args) {
-        MyArrayList2 list =new MyArrayList2();
-        list.add("寝室长");
-        list.add("刘浪雨");
-        list.add("小颜同志");
-        System.out.println(list);
-    }
-}
