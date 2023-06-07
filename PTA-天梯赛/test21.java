f2a5960547aa3e1f834e07da38d780538265fbb9
骚皮耗子 <118991670+SPHZ-LH@users.noreply.github.com>
2023-06-07 23:42:35 +0800

    Delete PTA-天梯赛 directory

--- PTA-天梯赛/test21.java
+++
@@ -1,7 +0,0 @@
-public class test21 {
-    public static void main(String[] args) {
-        System.out.println("I'm gonna WIN!");
-        System.out.println("I'm gonna WIN!");
-        System.out.println("I'm gonna WIN!");
-    }
-}
--- PTA-天梯赛/test38.java
+++
@@ -1,6 +0,0 @@
-public class test38 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello New World");
-    }
-}
