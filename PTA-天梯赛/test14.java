d87bef3d3a547f0026d8cbed1d2953a7efbee00b
骚皮耗子 <118991670+SPHZ-LH@users.noreply.github.com>
2023-06-14 23:03:12 +0800

    Delete PTA-天梯赛 directory

--- PTA-天梯赛/test14.java
+++
@@ -1,5 +0,0 @@
-public class test14 {
-    public static void main(String[] args) {
-        System.out.println("This is a simple problem.");
-    }
-}
--- PTA-天梯赛/test57.java
+++
@@ -1,5 +0,0 @@
-public class test57 {
-    public static void main(String[] args) {
-        System.out.println("PTA shi3 wo3 jing1 shen2 huan4 fa1 !");
-    }
-}
