e2c80556872fc10c31d00eeeab234b3b0582981d
骚皮耗子 <118991670+SPHZ-LH@users.noreply.github.com>
2023-07-28 23:38:42 +0800

    Delete draft directory

--- draft/draft8.java
+++
@@ -1,7 +0,0 @@
-public class draft8 {
-    public static void main(String[] args) {
-        String s= "asjdlaqingshizhangjqingshsdjalksiqingshizhangzhang";
-        int indexOf = s.indexOf("2");
-        System.out.println(indexOf);
-    }
-}
