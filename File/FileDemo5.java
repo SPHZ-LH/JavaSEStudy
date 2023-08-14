42b205b42b583466a2b1b50ef22e15c38b0443ce
骚皮耗子 <118991670+SPHZ-LH@users.noreply.github.com>
2023-08-14 23:12:39 +0800

    Delete File directory

--- File/FileDemo5.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.util.Arrays;
-
-public class FileDemo5 {
-    public static void main(String[] args) {
-        File[] arr = File.listRoots();
-        System.out.println(Arrays.toString(arr));
-    }
-}
