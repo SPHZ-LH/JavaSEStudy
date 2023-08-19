da54156308a2b779037b098db8a6a1cfafa342e6
骚皮耗子 <118991670+SPHZ-LH@users.noreply.github.com>
2023-08-19 23:11:53 +0800

    Delete API directory

--- API/Test11.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Test11 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String str = sc.next();
-        System.out.println(str.endsWith("?&&!"));
-    }
-}
--- API/test17.java
+++
@@ -1,8 +0,0 @@
-import java.time.Instant;
-
-public class test17 {
-    public static void main(String[] args) {
-        Instant s =Instant.now();
-        System.out.println(s);
-    }
-}
