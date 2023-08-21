515743fe20c9461e6eb26e22e153918ae48a598d
骚皮耗子 <118991670+SPHZ-LH@users.noreply.github.com>
2023-08-21 23:12:33 +0800

    Add files via upload

---
+++ API/test18.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class test18 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String str = sc.nextLine();
+        int num = Integer.parseInt(str);
+        System.out.println(num);
+    }
+}
