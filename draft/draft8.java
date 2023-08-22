fe014ad488a7c0acc07e6332b0ccf10fd3fbdd90
骚皮耗子 <118991670+SPHZ-LH@users.noreply.github.com>
2023-08-22 23:58:25 +0800

    Add files via upload

---
+++ draft/draft8.java
@@ -0,0 +1,7 @@
+public class draft8 {
+    public static void main(String[] args) {
+        String s= "asjdlaqingshizhangjqingshsdjalksiqingshizhangzhang";
+        int indexOf = s.indexOf("2");
+        System.out.println(indexOf);
+    }
+}
