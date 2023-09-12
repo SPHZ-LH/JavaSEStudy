753faa8a01dfdc0b994399c4353466018e89bbc1
骚皮耗子 <118991670+SPHZ-LH@users.noreply.github.com>
2023-09-12 23:18:12 +0800

    Delete API directory

--- API/test6.java
+++
@@ -1,7 +0,0 @@
-public class test6 {
-    public static void main(String[] args) {
-        String str="12312312";
-        String str1="\\d+";
-        System.out.println(str.matches(str1));
-    }
-}
