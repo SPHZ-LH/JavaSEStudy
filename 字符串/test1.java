3b6b550b99052dad567a666e7dde6c8330948a2e
骚皮耗子 <118991670+SPHZ-LH@users.noreply.github.com>
2023-09-19 23:30:26 +0800

    Delete 字符串 directory

--- 字符串/test1.java
+++
@@ -1,9 +0,0 @@
-public class test1 {
-    public static void main(String[] args) {
-        String num = "13523123242";
-        String HMnum = num.substring(0, 3);
-        String Endnum = num.substring(7);
-        String result = HMnum + "****" + Endnum;
-        System.out.println(result);
-    }
-}
--- 字符串/test2.java
+++
@@ -1,7 +0,0 @@
-public class test2 {
-    public static void main(String[] args) {
-        char num ='3';
-        int newNum =(int)(num);
-        System.out.println(newNum);
-    }
-}
